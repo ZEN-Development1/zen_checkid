c809fe660cfa6f79571214452900f5eb07905414
ZEN Development <127617452+ZEN-Development1@users.noreply.github.com>
2023-03-11 11:44:46 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "zen_checkid"
+description "A Custom Made FiveM resource that adds a /id chat command to show a player's ID."
+author "ZEN"
+
+-- Client script
+client_script 'client.lua'
+
+-- Server script
+server_scripts {
+  'server.lua'
+}
